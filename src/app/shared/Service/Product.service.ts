b80708dd03c83acfaf77f5802f5e00134bdcc05a
yasminetlili <yasmine.tlili@esprit.tn>
2022-11-15 10:54:02 +0100

    cmt10

--- src/app/shared/Service/Product.service.ts
+++ src/app/shared/Service/Product.service.ts
@@ -6,7 +6,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductService {
-  readonly API_URL = 'http://192.168.1.13:8089/SpringMVC/produit';
+  readonly API_URL = 'http://192.168.56.3:8080/SpringMVC/produit';
 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {
